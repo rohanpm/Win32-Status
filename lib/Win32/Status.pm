024839f817ac2f6588d7fd44804ae06462574333
Rohan McGovern <rohan.mcgovern@nokia.com>
2012-05-07 10:09:28 +1000

    Initial commit; unmodified output from module-starter.

---
+++ lib/Win32/Status.pm
@@ -0,0 +1,120 @@
+package Win32::Status;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Win32::Status - The great new Win32::Status!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Win32::Status;
+
+    my $foo = Win32::Status->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Rohan McGovern, C<< <rohan at mcgovern.id.au> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-win32-status at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Win32-Status>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Win32::Status
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Win32-Status>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Win32-Status>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Win32-Status>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Win32-Status/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Rohan McGovern.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+
+=cut
+
+1; # End of Win32::Status
